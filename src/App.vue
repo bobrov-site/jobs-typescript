4a97144ac3a1eac59d08947b694a410519afa25c
Daniil Bobrov <allodrmen@yandex.ru>
2023-07-19 15:38:57 +0300

    added buttons and styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <JobsList :jobs="jobs"/>
+    <div class="btns">
+      <button class="btn">Order by title</button>
+      <button class="btn">Order by salary</button>
+      <button class="btn">Order by location</button>
+    </div>
+    <JobsList :jobs="jobs" />
   </div>
 </template>
 
@@ -12,20 +17,40 @@ import Job from './types/Job'
 
 export default defineComponent({
   name: 'App',
-  components: {JobsList},
+  components: { JobsList },
   setup() {
     const jobs = ref<Job[]>([
-      {title: 'farm worker', location: 'Moscow', salary: 3000, id: '1'},
-      {title: 'fisher', location: 'Stavropol', salary: 4000, id: '2'},
-      {title: 'developer', location: 'Krasnodar', salary: 5000, id: '3'},
-      {title: 'ceo', location: 'Minsk', salary: 5000, id: '4'},
-      {title: 'ui designer', location: 'Kuban', salary: 6000, id: '4'}
+      { title: 'farm worker', location: 'Moscow', salary: 3000, id: '1' },
+      { title: 'fisher', location: 'Stavropol', salary: 4000, id: '2' },
+      { title: 'developer', location: 'Krasnodar', salary: 5000, id: '3' },
+      { title: 'ceo', location: 'Minsk', salary: 5000, id: '4' },
+      { title: 'ui designer', location: 'Kuban', salary: 6000, id: '4' }
     ])
-    return {jobs}
+    return { jobs }
   },
 });
 </script>
 
-<style>
+<style lang="css" scoped>
+.btns {
+  margin-left: 3rem;
+}
+.btn {
+  display: inline-block;
+  padding: 12px 24px;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  text-decoration: none;
+  background-color: #4CAF50;
+  color: #fff;
+  border-radius: 4px;
+  transition: background-color 0.3s ease;
+  margin-right: 2rem;
+}
 
+.btn:hover {
+  background-color: #45a049;
+  cursor: pointer;
+}
 </style>
