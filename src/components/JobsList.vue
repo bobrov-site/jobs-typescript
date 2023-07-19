a6da8804faf03bf0749022109d250d5ff2425578
Daniil Bobrov <allodrmen@yandex.ru>
2023-07-19 15:34:27 +0300

    added layout and style

--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,21 +1,72 @@
 <template>
     <div>
-
+        <ul>
+            <li v-for="job in jobs" :key="job.id" class="job">
+                <h2>{{ job.title }} in {{ job.location }}</h2>
+                <div class="salary">
+                    <span>{{ job.salary }}₽</span>
+                </div>
+                <div class="description">
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos quisquam, nisi exercitationem
+                        voluptas voluptates perspiciatis molestias vero, corporis facilis voluptate cumque quibusdam?
+                        Reiciendis blanditiis sunt culpa magni in cum! Nesciunt.
+                    </p>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
+import Job from '@/types/Job'
 
 export default defineComponent({
-    setup () {
-        
+    props: {
+        jobs: {
+            required: true,
+            type: Array as PropType<Job[]>
+        }
+    },
+    setup() {
+
 
         return {}
     }
 })
 </script>
 
-<style scoped>
+<style lang="css" scoped>
+ul {
+    list-style-type: none;
+}
 
+.job {
+    padding: 1rem;
+    background-color: #fff;
+    margin-bottom: 2rem;
+    border-radius: 15px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    transition: box-shadow 0.3s ease;
+}
+
+.job:hover {
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2), 0 6px 20px rgba(0, 0, 0, 0.2);
+    transform: translateY(-2px);
+}
+
+h2 {
+    margin-top: 0;
+}
+
+.salary {
+    color: green;
+    font-weight: bold;
+}
+
+.description {
+    margin-top: 1rem;
+    color: #888;
+}
 </style>
