da816cde0593278abeff951c7dd69372bf091482
Daniil Bobrov <allodrmen@yandex.ru>
2023-07-19 14:34:00 +0300

    added refs and reactive with typescript

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,28 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+// должен быть импорт для reactive и toRefs ⬇️
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'App',
   components: {},
-  data() {
-    return {
-      name: 'Link',
-      //можно добавить типы
-      age: 25 as number | string
-    }
+  setup() {
+    //использование через реактив
+
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: 25 as string | number
+    // })
+    // return {...toRefs(state)}
+
+    //использование через рефы
+    const name = ref('Link')
+    name.value = 'daniil'
+    const age = ref<number | string>(25)
+    age.value = 30
+
+    return {name, age}
   },
   methods: {
     changeName(name: string) {
