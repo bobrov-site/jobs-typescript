03be4b5b3a52eb0f104e6116434618dd6c556f46
Daniil Bobrov <allodrmen@yandex.ru>
2023-07-19 14:36:07 +0300

    commented and removes methods

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <p>{{ name }} - {{ age }}</p>
-    <button @click="changeName('zelda' )">change name</button>
-    <button @click="changeAge('13' )">change age</button>
+    
   </div>
 </template>
 
@@ -23,21 +21,13 @@ export default defineComponent({
     // return {...toRefs(state)}
 
     //использование через рефы
-    const name = ref('Link')
-    name.value = 'daniil'
-    const age = ref<number | string>(25)
-    age.value = 30
+    // const name = ref('Link')
+    // name.value = 'daniil'
+    // const age = ref<number | string>(25)
+    // age.value = 30
 
-    return {name, age}
+    // return {name, age}
   },
-  methods: {
-    changeName(name: string) {
-      this.name = name
-    },
-    changeAge(age: number | string) {
-      this.age = age
-    }
-  }
 });
 </script>
 
