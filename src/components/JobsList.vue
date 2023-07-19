ff5c0c77666fabfd43fa425e523dff6ee6017ab3
Daniil Bobrov <allodrmen@yandex.ru>
2023-07-19 19:09:38 +0300

    added setup for JobsList

--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
+        <p>Ordered by {{ order }}</p>
         <ul>
-            <li v-for="job in jobs" :key="job.id" class="job">
+            <li v-for="job in orderedJobs" :key="job.id" class="job">
                 <h2>{{ job.title }} in {{ job.location }}</h2>
                 <div class="salary">
                     <span>{{ job.salary }}₽</span>
@@ -19,20 +20,28 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
 import Job from '@/types/Job'
+import OrderTerm from '@/types/OrderTerm';
 
 export default defineComponent({
     props: {
         jobs: {
             required: true,
             type: Array as PropType<Job[]>
+        },
+        order: {
+            required: true,
+            type: String as PropType<OrderTerm>
         }
     },
-    setup() {
-
-
-        return {}
+    setup(props) {
+        const orderedJobs = computed(() => {
+            return [...props.jobs].sort((a: Job, b: Job) => {
+                return a[props.order] > b[props.order] ? 1 : -1
+            })
+        })
+        return { orderedJobs }
     }
 })
 </script>
