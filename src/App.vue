e4df694f446a5de57bd5218ed6140afea8541e6d
Daniil Bobrov <allodrmen@yandex.ru>
2023-07-19 14:22:26 +0300

    added data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    
+    <p>{{ name }}</p>
+    <button @click="changeName('zelda' )">change name</button>
   </div>
 </template>
 
@@ -9,8 +10,18 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    
+  components: {},
+  data() {
+    return {
+      name: 'Link',
+      //можно добавить типы
+      age: 25 as number | string
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name
+    }
   }
 });
 </script>
